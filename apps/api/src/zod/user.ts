2cf2be233ebb853b05467b15fe8a9751aca38530
clark.zou <clark.zou@zoom.us>
2024-10-16 09:46:02 +0800

    chore: support auto zod schema

---
+++ apps/api/src/zod/user.ts
@@ -0,0 +1,23 @@
+import * as z from 'zod'
+
+import type { CompletePost } from './index'
+import { RelatedPostModel } from './index'
+
+export const UserModel = z.object({
+  id: z.number().int(),
+  email: z.string(),
+  name: z.string().nullish(),
+})
+
+export interface CompleteUser extends z.infer<typeof UserModel> {
+  posts: CompletePost[]
+}
+
+/**
+ * RelatedUserModel contains all relations on your model in addition to the scalars
+ *
+ * NOTE: Lazy required in case of potential circular dependencies within schema
+ */
+export const RelatedUserModel: z.ZodSchema<CompleteUser> = z.lazy(() => UserModel.extend({
+  posts: RelatedPostModel.array(),
+}))

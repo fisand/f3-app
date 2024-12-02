33c4fca0066842a13715e074e6a90c40157643b5
zouhang <1102639563@qq.com>
2024-12-03 00:14:03 +0800

    chore: update logic

--- packages/eslint-config/node.mjs
+++ packages/eslint-config/node.mjs
@@ -2,16 +2,10 @@ import { defineConfig } from 'eslint-config-hyoban'
 
 // todo custom
 export default defineConfig({
-  react: 'vite',
-  restrictedSyntax: ['jsx', 'tsx'],
+  react: 'next',
   strict: true,
   tailwindCSS: false,
   unocss: true,
 }, {
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
-    'react-refresh/only-export-components': 'off',
-  },
   ignores: ['**/*.d.ts'],
 })

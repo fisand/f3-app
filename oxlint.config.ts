6e9891adeeed3620a09f99bf458eddd3aed1c552
clark.zou <clark.zou@zoom.us>
2026-04-13 17:10:26 +0800

    fix: update oxlint configuration to enhance linting rules and environment settings

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'oxlint'
 
 export default defineConfig({
+  plugins: ['eslint', 'typescript', 'unicorn', 'oxc', 'react'],
+  env: {
+    browser: true,
+  },
+  ignorePatterns: ['dist'],
+  rules: {
+    'react/rules-of-hooks': 'error',
+    'react/exhaustive-deps': 'warn',
+    'react/only-export-components': [
+      'warn',
+      {
+        allowConstantExport: true,
+      },
+    ],
+  },
   options: {
     typeAware: true,
     typeCheck: true,
